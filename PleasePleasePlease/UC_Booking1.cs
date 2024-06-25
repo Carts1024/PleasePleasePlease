9197f2a4a81e51178c177b1dfed53351c9241616
akiszn <138987400+akiszn@users.noreply.github.com>
2024-06-25 13:22:28 +0800

    Update UC_Booking1.cs

--- PleasePleasePlease/UC_Booking1.cs
+++ PleasePleasePlease/UC_Booking1.cs
@@ -1,4 +1,4 @@
-﻿using Mirai_Paradise_Hotel;
+using Mirai_Paradise_Hotel;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -21,6 +21,8 @@ namespace PleasePleasePlease
         private void labelListofBooking_Click(object sender, EventArgs e)
         {
             panelListofBooking.Visible = true;
+            panel1.Visible = true;
+            panel2.Visible = true;
         }
 
         private void labelAddBook_Click(object sender, EventArgs e)
@@ -48,6 +50,45 @@ namespace PleasePleasePlease
             bookAdded.Show();
         }
 
+        private void buttonExitEditGuest_Click(object sender, EventArgs e)
+        {
+            buttonSaveEditBookings.Visible = false;
+            buttonExitEditBookings.Visible = false;
+            ColumnCheckInDate.ReadOnly = true;
+            ColumnCheckInTime.ReadOnly = true;
+            ColumnCheckOutDate.ReadOnly = true;
+            ColumnCheckOutTime.ReadOnly = true;
+        }
 
+        private void buttonSaveEditBookings_Click(object sender, EventArgs e)
+        {
+            // Alter Information in Database and Save code starts here
+
+            buttonSaveEditBookings.Visible = false;
+            buttonExitEditBookings.Visible = false;
+            ColumnCheckInDate.ReadOnly = true;
+            ColumnCheckInTime.ReadOnly = true;
+            ColumnCheckOutDate.ReadOnly = true;
+            ColumnCheckOutTime.ReadOnly = true;
+            Dialogue_BookingUpdated bookingUpdated = new Dialogue_BookingUpdated();
+            bookingUpdated.Show();
+        }
+
+        private void buttonEditBookings_Click(object sender, EventArgs e)
+        {
+            buttonSaveEditBookings.Visible = true;
+            buttonExitEditBookings.Visible = true;
+            ColumnCheckInDate.ReadOnly = false;
+            ColumnCheckInTime.ReadOnly = false;
+            ColumnCheckOutDate.ReadOnly = false;
+            ColumnCheckOutTime.ReadOnly = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            panelListofBooking.Visible = false;
+            panel1.Visible = false;
+            panel2.Visible = false;
+        }
     }
 }
